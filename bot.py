a27d494b57507fe3610bfd56ffc904f8accc2b61
vjspranav <pranavasri@live.in>
2021-05-12 02:17:41 +0530

    Create a user and superuser restriction

--- bot.py
+++ bot.py
@@ -17,9 +17,9 @@ import json
 config={}
 with open("config.json") as json_config_file:
     config = json.load(json_config_file)
+restricted={}
     
 token = config["telegram"]["token"]
-
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -31,9 +31,40 @@ print(bot.get_me())
 updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 
+def superuser_restricted(func):
+    """Restrict usage of func to allowed users only and replies if necessary"""
+    with open("restricted.json") as json_config_file:
+        restricted = json.load(json_config_file)
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if str(user_id) not in restricted['superuser'].values():
+            print(user_id, " is not in superuser")
+            print("WARNING: Unauthorized access denied for {}.".format(user_id))
+            update.message.reply_text('Not Superuser, incident will be recorded')
+            return  # quit function
+        return func(update, context, *args, **kwargs)
+    return wrapped
+
+def user_restricted(func):
+    """Restrict usage of func to allowed users only and replies if necessary"""
+    with open("restricted.json") as json_config_file:
+        restricted = json.load(json_config_file)
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if str(user_id) not in restricted['user'].values():
+            print(user_id, " is not in accessible users")
+            print("WARNING: Unauthorized access denied for {}.".format(user_id))
+            update.message.reply_text('User disallowed.')
+            return  # quit function
+        return func(update, context, *args, **kwargs)
+    return wrapped
+
+
 def start(update, context):
     user_id = update.effective_chat.id
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Welcome to the Jenkins bot.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Welcome to the Linux Management Bot.")
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
